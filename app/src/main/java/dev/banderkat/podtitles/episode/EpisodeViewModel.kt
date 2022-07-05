2fe2009d399a85c26470e4c98741c30bbcbda64c
Kathryn Killebrew <kathryn.killebrew>
2022-07-05 17:47:18 -0400

    Remove bottom navigation

--- app/src/main/java/dev/banderkat/podtitles/episode/EpisodeViewModel.kt
+++ app/src/main/java/dev/banderkat/podtitles/episode/EpisodeViewModel.kt
@@ -1,13 +1,13 @@
-package dev.banderkat.podtitles.ui.notifications
+package dev.banderkat.podtitles.episode
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class EpisodeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is episode Fragment"
     }
     val text: LiveData<String> = _text
 }
