2d1e922c453db69f046ff7736ddb03464ac53100
Kathryn Killebrew <kathryn.killebrew>
2022-07-05 00:14:03 -0400

    Move transcription to background worker

--- app/src/main/java/dev/banderkat/podtitles/ui/home/HomeFragment.kt
+++ app/src/main/java/dev/banderkat/podtitles/ui/home/HomeFragment.kt
@@ -12,7 +12,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.media3.common.C.SELECTION_FLAG_AUTOSELECT
 import androidx.media3.common.C.TRACK_TYPE_DEFAULT
 import androidx.media3.common.MediaItem
@@ -24,6 +23,7 @@ import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.offline.DownloadRequest
 import androidx.media3.exoplayer.offline.DownloadService
 import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.work.*
 import com.arthenica.ffmpegkit.FFmpegKit
 import com.arthenica.ffmpegkit.FFmpegKitConfig
 import com.arthenica.ffmpegkit.Session
@@ -32,36 +32,15 @@ import dev.banderkat.podtitles.PodTitlesApplication
 import dev.banderkat.podtitles.databinding.FragmentHomeBinding
 import dev.banderkat.podtitles.player.DOWNLOAD_FINISHED_ACTION
 import dev.banderkat.podtitles.player.PodTitlesDownloadService
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.json.JSONObject
-import org.vosk.Model
-import org.vosk.Recognizer
-import org.vosk.android.RecognitionListener
-import org.vosk.android.SpeechStreamService
-import org.vosk.android.StorageService
-import org.w3c.dom.Document
-import org.w3c.dom.Element
+import dev.banderkat.podtitles.workers.AUDIO_FILE_PATH_PARAM
+import dev.banderkat.podtitles.workers.SUBTITLE_FILE_PATH_PARAM
+import dev.banderkat.podtitles.workers.TranscribeWorker
 import java.io.File
-import java.io.FileInputStream
-import java.io.IOException
-import java.io.StringWriter
-import javax.xml.parsers.DocumentBuilderFactory
-import javax.xml.transform.ErrorListener
-import javax.xml.transform.TransformerException
-import javax.xml.transform.TransformerFactory
-import javax.xml.transform.dom.DOMSource
-import javax.xml.transform.stream.StreamResult
 
 const val MEDIA_URI = "https://storage.googleapis.com/exoplayer-test-media-0/play.mp3"
 const val FFMPEG_PARAMS = "-ac 1 -ar 16000"
-const val VOSK_HZ = 16000.0f
-const val VOSK_MODEL_ASSET = "model-en-us"
-const val VOSK_MODEL_NAME = "model"
-const val SUBTITLE_FILE_NAME = "subtitles.ttml"
 
-class HomeFragment : Fragment(), RecognitionListener {
+class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -72,15 +51,7 @@ class HomeFragment : Fragment(), RecognitionListener {
 
     private var mediaItem: MediaItem? = null
     private var ffmpegSession: Session? = null
-
-    private var voskModel: Model? = null
-    private var voskRecognizer: Recognizer? = null
-    private var voskSpeechStreamService: SpeechStreamService? = null
-
-    private val xmlDocumentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-    private var subtitleDocument: Document? = xmlDocumentBuilder.newDocument()
-    private var ttmlParent: Element? = null
-    private var resultsCounter = 0
+    private var subtitleFilePath: String? = null
 
     private val downloadCompleteBroadcast: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
@@ -118,7 +89,6 @@ class HomeFragment : Fragment(), RecognitionListener {
             IntentFilter(DOWNLOAD_FINISHED_ACTION)
         )
 
-        initializeVosk()
         sendDownloadRequest()
 
         // initializePlayer()
@@ -132,9 +102,6 @@ class HomeFragment : Fragment(), RecognitionListener {
     override fun onStop() {
         super.onStop()
         releasePlayer()
-
-        voskSpeechStreamService?.stop()
-        voskSpeechStreamService = null
     }
 
     override fun onDestroyView() {
@@ -195,21 +162,8 @@ class HomeFragment : Fragment(), RecognitionListener {
 
         if (mediaItem == null || player == null) return
 
-        val absPath = requireContext().getFileStreamPath(SUBTITLE_FILE_NAME).absolutePath
-        Log.d("Player", "Absolute path to subtitles: $absPath")
-        val subtitleUri = Uri.fromFile(File(absPath))
+        val subtitleUri = Uri.fromFile(File(subtitleFilePath))
         Log.d("Player", "Got path to subtitles: $subtitleUri")
-
-
-
-        requireContext().openFileInput(SUBTITLE_FILE_NAME).use { stream ->
-            val text = stream.bufferedReader().use {
-                it.readText()
-            }
-
-            Log.d("Player", "Read from subtitles file: ${text}")
-        }
-
         Log.d("Player", "Existing media item URI: ${mediaItem?.localConfiguration?.uri}")
 
         val subtitle = MediaItem.SubtitleConfiguration.Builder(subtitleUri)
@@ -255,134 +209,50 @@ class HomeFragment : Fragment(), RecognitionListener {
                 "ffmpeg finished. return code: ${it.returnCode} duration: ${it.duration}"
             )
 
+            // TODO: cleanup. delete wav file once transcription complete
+
             if (it.failStackTrace != null) {
                 Log.e("ffmpeg", "ffmpeg failed. ${it.failStackTrace}")
             } else {
-                lifecycleScope.launch {
-                    recognizeVosk(outputFilePath)
+                // launch transcription worker
+                val transcribeRequest = OneTimeWorkRequestBuilder<TranscribeWorker>()
+                    .setInputData(workDataOf(AUDIO_FILE_PATH_PARAM to outputFilePath))
+                    .setConstraints(Constraints.Builder().setRequiresStorageNotLow(true).build())
+                    .addTag("transcribe") // TODO: move
+                    .build()
+
+                val workManager = WorkManager.getInstance(requireContext())
+                workManager.enqueue(transcribeRequest)
+
+                requireActivity().runOnUiThread {
+                    workManager
+                        .getWorkInfoByIdLiveData(transcribeRequest.id)
+                        .observe(viewLifecycleOwner) { workInfo ->
+                            when (workInfo?.state) {
+                                WorkInfo.State.SUCCEEDED -> {
+                                    val subtitlePath = workInfo.outputData.getString(
+                                        SUBTITLE_FILE_PATH_PARAM
+                                    )
+                                    Log.d(
+                                        "HomeFragment",
+                                        "Transcription worker successfully wrote file $subtitlePath"
+                                    )
+                                    subtitleFilePath = subtitlePath
+                                    startPlayer()
+                                }
+                                WorkInfo.State.FAILED -> {
+                                    Log.e("HomeFragment", "Transcription worker failed")
+                                }
+                                else -> {
+                                    Log.d(
+                                        "HomeFragment",
+                                        "Transcription worker moved to state ${workInfo?.state}"
+                                    )
+                                }
+                            }
+                        }
                 }
             }
         }
-    }
-
-    private fun initializeVosk() {
-        try {
-            val assetPath = "$VOSK_MODEL_ASSET"
-            val outputPath = StorageService.sync(requireContext(), assetPath, VOSK_MODEL_NAME)
-            voskModel = Model(outputPath)
-            Log.d("Vosk", "Vosk model unpacked successfully")
-        } catch (e: IOException) {
-            Log.e("Vosk", "Failed to unpack model", e)
-            voskModel = null
-        }
-    }
-
-    private suspend fun recognizeVosk(inputFilePath: String) = withContext(Dispatchers.IO) {
-        // see TTML format docs: https://www.w3.org/TR/ttml2/
-        subtitleDocument = xmlDocumentBuilder.newDocument()
-        subtitleDocument?.apply {
-            val tt = createElement("tt")
-            val ttAttr = createAttribute("xmlns")
-            ttAttr.value = "http://www.w3.org/ns/ttml"
-            val body = createElement("body")
-            val regionAttr = createAttribute("region")
-            regionAttr.value = "subtitleArea"
-            body.setAttributeNode(regionAttr)
-            tt.appendChild(body)
-            appendChild(tt)
-
-            ttmlParent = createElement("div")
-            body.appendChild(ttmlParent)
-        }
-
-        Log.d("Vosk", "creating Vosk recognizer")
-        voskRecognizer = Recognizer(voskModel, VOSK_HZ)
-        voskRecognizer?.setWords(true) // include timestamps on results
-        val fileStream = FileInputStream(inputFilePath)
-        voskSpeechStreamService = SpeechStreamService(voskRecognizer, fileStream, VOSK_HZ)
-        Log.d("Vosk", "starting Vosk stream service")
-        voskSpeechStreamService?.start(this@HomeFragment)
-    }
-
-    // Vosk listener methods
-    override fun onPartialResult(hypothesis: String?) {
-        Log.d("Vosk", "partial result: $hypothesis")
-    }
-
-    override fun onResult(hypothesis: String?) {
-        Log.d("Vosk", "result: $hypothesis")
-        if (hypothesis.isNullOrEmpty()) return
-
-        val json = JSONObject(hypothesis)
-        val resultText = json.getString("text")
-        if (resultText.isNullOrEmpty()) return
-
-        resultsCounter++
-        val resultJson = json.getJSONArray("result")
-        subtitleDocument?.apply {
-            val paragraph = createElement("p")
-            val idAttr = createAttribute("xml:id")
-            idAttr.value = "subtitle$resultsCounter"
-            paragraph.setAttributeNode(idAttr)
-            val beginAttr = createAttribute("begin")
-            val endAttr = createAttribute("end")
-            val firstWord = resultJson.getJSONObject(0)
-            val lastWord = resultJson.getJSONObject(resultJson.length() - 1)
-            val startStr = String.format("%.2f", firstWord.get("start"))
-            val endStr = String.format("%.2f", lastWord.get("end"))
-            beginAttr.value = "${startStr}s"
-            endAttr.value = "${endStr}s"
-            paragraph.setAttributeNode(beginAttr)
-            paragraph.setAttributeNode(endAttr)
-            paragraph.appendChild(createTextNode(resultText))
-            ttmlParent?.appendChild(paragraph)
-        }
-    }
-
-    override fun onFinalResult(hypothesis: String?) {
-        Log.d("Vosk", "final result: $hypothesis")
-
-        val transformer = TransformerFactory.newInstance().newTransformer()
-        transformer.errorListener = object : ErrorListener {
-            override fun warning(p0: TransformerException?) {
-                Log.w("Vosk", "transformer warning", p0)
-            }
-
-            override fun error(p0: TransformerException?) {
-                Log.e("Vosk", "transformer error", p0)
-            }
-
-            override fun fatalError(p0: TransformerException?) {
-                Log.e("Vosk", "transformer fatal error", p0)
-            }
-        }
-
-        // val app = requireActivity().application as PodTitlesApplication
-        // FIXME
-
-        requireContext().openFileOutput(SUBTITLE_FILE_NAME, Context.MODE_PRIVATE).use {
-            val writer = StreamResult(StringWriter())
-
-            val result = StreamResult(it)
-            val domSource = DOMSource(subtitleDocument)
-
-            transformer.transform(domSource, writer)
-
-            val xmlStr = writer.writer.toString()
-            Log.d("Vosk", "generated subtitle doc:")
-            Log.d("Vosk", xmlStr)
-
-            transformer.transform(domSource, result)
-        }
-
-        startPlayer()
-    }
-
-    override fun onError(exception: Exception?) {
-        Log.e("Vosk", "Vosk processing error", exception)
-    }
-
-    override fun onTimeout() {
-        Log.e("Vosk", "Vosk timed out")
     }
 }
---
+++ app/src/main/java/dev/banderkat/podtitles/workers/TranscribeWorker.kt
@@ -0,0 +1,172 @@
+package dev.banderkat.podtitles.workers
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Data
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.google.common.collect.ImmutableMap
+import org.json.JSONObject
+import org.vosk.Model
+import org.vosk.Recognizer
+import org.vosk.android.StorageService
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import java.io.File
+import java.io.FileInputStream
+import java.io.StringWriter
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.ErrorListener
+import javax.xml.transform.TransformerException
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+import kotlin.math.roundToInt
+
+const val TAG = "TranscribeWorker"
+const val SAMPLE_RATE = 16000.0f
+const val BUFFER_SIZE_SECONDS = 0.2f
+const val VOSK_MODEL_ASSET = "model-en-us"
+const val VOSK_MODEL_NAME = "model"
+const val SUBTITLE_FILE_EXTENSION = ".ttml"
+const val AUDIO_FILE_PATH_PARAM = "input_audio_path"
+const val SUBTITLE_FILE_PATH_PARAM = "output_ttml_path"
+
+/**
+ * Transcribe a cached audio chunk with Vosk and convert the results to a TTML subtitle file.
+ */
+class TranscribeWorker(appContext: Context, workerParams: WorkerParameters) :
+    Worker(appContext, workerParams) {
+
+    private val xmlDocumentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+    private var subtitleDocument: Document? = xmlDocumentBuilder.newDocument()
+    private var ttmlParent: Element? = null
+    private var resultsCounter = 0
+
+    override fun doWork(): Result {
+        try {
+            val inputPath = inputData.getString(AUDIO_FILE_PATH_PARAM) ?: return Result.failure()
+            createSubtitleDocument()
+            val outputPath = recognize(inputPath)
+            return Result.success(Data(ImmutableMap.of(SUBTITLE_FILE_PATH_PARAM, outputPath)))
+        } catch (ex: Exception) {
+            Log.e(TAG, "Vosk transcription failed", ex)
+            return Result.failure()
+        }
+    }
+
+    // based on:
+    // https://github.com/alphacep/vosk-api/blob/master/android/lib/src/main/java/org/vosk/android/SpeechStreamService.java
+    private fun recognize(inputFilePath: String): String {
+        val model =
+            Model(StorageService.sync(applicationContext, VOSK_MODEL_ASSET, VOSK_MODEL_NAME))
+        val recognizer = Recognizer(model, SAMPLE_RATE)
+        recognizer.setWords(true) // include timestamps
+
+        val inputStream = FileInputStream(inputFilePath)
+        val bufferSize = (SAMPLE_RATE * BUFFER_SIZE_SECONDS * 2).roundToInt()
+        val buffer = ByteArray(bufferSize)
+
+        do {
+            val numberRead = inputStream.read(buffer, 0, bufferSize)
+            val isSilence = recognizer.acceptWaveForm(buffer, bufferSize)
+            // ignore partial results (when silence not found)
+            if (isSilence) {
+                handleResult(recognizer.result)
+            }
+        } while (numberRead >= 0)
+
+        // output file path is the same as the input, but with the subtitle extension
+        val inputFile = File(inputFilePath)
+        val outputFilePath = "${inputFile.nameWithoutExtension}${SUBTITLE_FILE_EXTENSION}"
+
+        handleFinalResult(recognizer.finalResult, outputFilePath)
+        return applicationContext.getFileStreamPath(outputFilePath).absolutePath
+    }
+
+    private fun createSubtitleDocument() {
+        // see TTML format docs: https://www.w3.org/TR/ttml2/
+        subtitleDocument = xmlDocumentBuilder.newDocument()
+        subtitleDocument?.apply {
+            val tt = createElement("tt")
+            val ttAttr = createAttribute("xmlns")
+            ttAttr.value = "http://www.w3.org/ns/ttml"
+            val body = createElement("body")
+            val regionAttr = createAttribute("region")
+            regionAttr.value = "subtitleArea"
+            body.setAttributeNode(regionAttr)
+            tt.appendChild(body)
+            appendChild(tt)
+
+            ttmlParent = createElement("div")
+            body.appendChild(ttmlParent)
+        }
+    }
+
+    private fun handleResult(hypothesis: String?) {
+        Log.d(TAG, "Vosk result: $hypothesis")
+        if (hypothesis.isNullOrEmpty()) return
+
+        val json = JSONObject(hypothesis)
+        val resultText = json.getString("text")
+        if (resultText.isNullOrEmpty()) return
+
+        resultsCounter++
+        val resultJson = json.getJSONArray("result")
+        subtitleDocument?.apply {
+            val paragraph = createElement("p")
+            val idAttr = createAttribute("xml:id")
+            idAttr.value = "subtitle$resultsCounter"
+            paragraph.setAttributeNode(idAttr)
+            val beginAttr = createAttribute("begin")
+            val endAttr = createAttribute("end")
+            val firstWord = resultJson.getJSONObject(0)
+            val lastWord = resultJson.getJSONObject(resultJson.length() - 1)
+            val startStr = String.format("%.2f", firstWord.get("start"))
+            val endStr = String.format("%.2f", lastWord.get("end"))
+            beginAttr.value = "${startStr}s"
+            endAttr.value = "${endStr}s"
+            paragraph.setAttributeNode(beginAttr)
+            paragraph.setAttributeNode(endAttr)
+            paragraph.appendChild(createTextNode(resultText))
+            ttmlParent?.appendChild(paragraph)
+        }
+    }
+
+    private fun handleFinalResult(hypothesis: String?, outputFilePath: String) {
+        Log.d(TAG, "Vosk final result: $hypothesis")
+
+        val transformer = TransformerFactory.newInstance().newTransformer()
+        transformer.errorListener = object : ErrorListener {
+            override fun warning(warning: TransformerException?) {
+                Log.w(TAG, "XML transformer warning", warning)
+            }
+
+            override fun error(ex: TransformerException?) {
+                Log.e(TAG, "XML transformer error", ex)
+            }
+
+            override fun fatalError(ex: TransformerException?) {
+                Log.e(TAG, "XML transformer fatal error", ex)
+                if (ex != null) throw ex
+            }
+        }
+
+        // write to file
+        applicationContext.openFileOutput(outputFilePath, Context.MODE_PRIVATE).use {
+            // TODO: remove debug logging
+            val writer = StreamResult(StringWriter())
+
+            val result = StreamResult(it)
+            val domSource = DOMSource(subtitleDocument)
+
+            transformer.transform(domSource, writer)
+
+            val xmlStr = writer.writer.toString()
+            Log.d("Vosk", "generated subtitle doc:")
+            Log.d("Vosk", xmlStr)
+
+            transformer.transform(domSource, result)
+        }
+    }
+}
