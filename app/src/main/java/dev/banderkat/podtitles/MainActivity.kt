119c44312c5bc62fbb2cae1e2669225caeac6ea0
Kathryn Killebrew <kathryn.killebrew>
2022-07-10 21:36:17 -0400

    Debounce search queries

--- app/src/main/java/dev/banderkat/podtitles/MainActivity.kt
+++ app/src/main/java/dev/banderkat/podtitles/MainActivity.kt
@@ -21,11 +21,17 @@ import dev.banderkat.podtitles.workers.AUTHORS_RESULT_KEY
 import dev.banderkat.podtitles.workers.PODCAST_QUERY_PARAM
 import dev.banderkat.podtitles.workers.PodcastSearchWorker
 import dev.banderkat.podtitles.workers.TITLES_RESULT_KEY
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val TAG = "MainActivity"
+        const val SEARCH_WORK_TAG = "podtitles_search"
+        const val SEARCH_DEBOUNCE_MS = 500L
     }
+
+    private val workManager = WorkManager.getInstance(this)
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var cursorAdapter: CursorAdapter
@@ -47,17 +53,19 @@ class MainActivity : AppCompatActivity() {
         val searchItem: MenuItem? = menu.findItem(R.id.action_search_podcasts)
         val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
         searchView = searchItem?.actionView as SearchView
+
         searchView.apply {
             setSearchableInfo(searchManager.getSearchableInfo(componentName))
-
-            val from = arrayOf(SearchManager.SUGGEST_COLUMN_TEXT_1, SearchManager.SUGGEST_COLUMN_TEXT_2)
+            val from =
+                arrayOf(SearchManager.SUGGEST_COLUMN_TEXT_1, SearchManager.SUGGEST_COLUMN_TEXT_2)
             val to = intArrayOf(R.id.search_result_title, R.id.search_results_subtitle)
             cursorAdapter = SimpleCursorAdapter(
                 context, R.layout.search_result_item, null,
-                from, to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER)
+                from, to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
+            )
             suggestionsAdapter = cursorAdapter
 
-            setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     Log.d(TAG, "query text $query submitted")
                     // TODO: hide keyboard?
@@ -66,21 +74,26 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onQueryTextChange(query: String?): Boolean {
                     Log.d(TAG, "Query text changed to $query")
+                    if (query.isNullOrEmpty()) return true
 
-                    val cursor = MatrixCursor(arrayOf(BaseColumns._ID,
-                        SearchManager.SUGGEST_COLUMN_TEXT_1, SearchManager.SUGGEST_COLUMN_TEXT_2))
+                    val cursor = MatrixCursor(
+                        arrayOf(
+                            BaseColumns._ID,
+                            SearchManager.SUGGEST_COLUMN_TEXT_1, SearchManager.SUGGEST_COLUMN_TEXT_2
+                        )
+                    )
                     val loading = getString(R.string.loading)
                     cursor.addRow(arrayOf(0, loading, ""))
 
                     suggestionsAdapter.changeCursor(cursor)
 
-                    if (!query.isNullOrEmpty()) searchPodcasts(query)
+                    searchPodcasts(query)
                     return true
                 }
             })
 
             setIconifiedByDefault(false)
-            setOnSuggestionListener(object: SearchView.OnSuggestionListener {
+            setOnSuggestionListener(object : SearchView.OnSuggestionListener {
                 override fun onSuggestionSelect(position: Int): Boolean {
                     Log.d(TAG, "suggestion selected at $position")
                     return false
@@ -97,15 +110,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun searchPodcasts(query: String) {
         Log.d(TAG, "Go search for podcasts with $query")
+
+        // cancel any previous searches
+        workManager.cancelAllWorkByTag(SEARCH_WORK_TAG)
+
         val searchRequest = OneTimeWorkRequestBuilder<PodcastSearchWorker>()
+            .setInitialDelay(SEARCH_DEBOUNCE_MS, TimeUnit.MILLISECONDS) // debounce
             .setInputData(workDataOf(PODCAST_QUERY_PARAM to query))
             .setConstraints(Constraints.Builder().setRequiresStorageNotLow(true).build())
-            .addTag("search") // TODO: move
+            .addTag(SEARCH_WORK_TAG)
             .build()
 
-        val workManager = WorkManager.getInstance(this)
         workManager.enqueue(searchRequest)
-
         workManager
             .getWorkInfoByIdLiveData(searchRequest.id)
             .observe(this) { workInfo ->
@@ -115,8 +131,13 @@ class MainActivity : AppCompatActivity() {
                         val titles = workInfo.outputData.getStringArray(TITLES_RESULT_KEY)
                         val authors = workInfo.outputData.getStringArray(AUTHORS_RESULT_KEY)
 
-                        val cursor = MatrixCursor(arrayOf(BaseColumns._ID,
-                            SearchManager.SUGGEST_COLUMN_TEXT_1, SearchManager.SUGGEST_COLUMN_TEXT_2))
+                        val cursor = MatrixCursor(
+                            arrayOf(
+                                BaseColumns._ID,
+                                SearchManager.SUGGEST_COLUMN_TEXT_1,
+                                SearchManager.SUGGEST_COLUMN_TEXT_2
+                            )
+                        )
                         query.let {
                             titles?.forEachIndexed { index, suggestion ->
                                 if (suggestion.contains(query, true))
