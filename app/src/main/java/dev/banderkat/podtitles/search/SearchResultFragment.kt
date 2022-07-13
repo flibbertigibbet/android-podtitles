b5872deb4ae08b1bfccf016d41e16c742e82d8e4
Kathryn Killebrew <kathryn.killebrew>
2022-07-12 20:59:46 -0400

    Improve search result image loading
    Display circular loader while fetching.
    Also convert http to https in logo links.

--- app/src/main/java/dev/banderkat/podtitles/search/SearchResultFragment.kt
+++ app/src/main/java/dev/banderkat/podtitles/search/SearchResultFragment.kt
@@ -1,5 +1,6 @@
 package dev.banderkat.podtitles.search
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,16 +9,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 import dev.banderkat.podtitles.R
 import dev.banderkat.podtitles.databinding.FragmentSearchResultBinding
 import dev.banderkat.podtitles.models.GpodderSearchResult
 import dev.banderkat.podtitles.utils.AddFeed
+import java.net.URL
 
 class SearchResultFragment : Fragment() {
     companion object {
         const val TAG = "SearchResultFragment"
+        const val GLIDE_LOADER_STROKE_WIDTH = 5f
+        const val GLIDE_LOADER_CENTER_RADIUS = 30f
     }
 
     private var _binding: FragmentSearchResultBinding? = null
@@ -68,11 +73,26 @@ class SearchResultFragment : Fragment() {
                 }
             }
 
-            if (!searchResult.logoUrl.isNullOrEmpty()) {
+            if (searchResult.logoUrl.isNullOrEmpty()) {
+                searchResultImage.setImageResource(R.drawable.ic_headphones)
+            } else {
+                // ensure URL is HTTPS before attempting to load it
+                val httpsLogoUri = Uri.parse(searchResult.logoUrl)
+                    .buildUpon()
+                    .scheme("https")
+                    .build()
+                    .toString()
+
+                val circularProgressDrawable = CircularProgressDrawable(requireContext())
+                circularProgressDrawable.strokeWidth = GLIDE_LOADER_STROKE_WIDTH
+                circularProgressDrawable.centerRadius = GLIDE_LOADER_CENTER_RADIUS
+                circularProgressDrawable.start()
+
                 Glide.with(this@SearchResultFragment)
-                    .load(searchResult.logoUrl)
-                    .placeholder(R.drawable.ic_headphones)
+                    .load(httpsLogoUri)
+                    .placeholder(circularProgressDrawable)
                     .fitCenter()
+                    .error(R.drawable.ic_headphones)
                     .into(searchResultImage)
             }
         }
