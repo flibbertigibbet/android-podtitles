36a8c1ed6692c9999efa953e5ce5b15c30c88614
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2022-07-17 00:50:27 -0400

    Add Glide module

---
+++ app/src/main/java/dev/banderkat/podtitles/utils/PodTitlesGlideModule.kt
@@ -0,0 +1,11 @@
+package dev.banderkat.podtitles.utils
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Expected app module for Glide. See:
+ * https://bumptech.github.io/glide/doc/generatedapi.html#kotlin
+ */
+@GlideModule
+class PodTitlesGlideModule: AppGlideModule() {}
